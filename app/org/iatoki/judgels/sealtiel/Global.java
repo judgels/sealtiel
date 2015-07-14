8231a62ed7a0620789db2e1cbd58221b135c4756
Jordan Fernando <fernandojordan.92@gmail.com>
2015-07-14 10:07:38 +0700

    Adapt to Judgels Play data migration
    part of ia-toki/judgels#294

--- app/org/iatoki/judgels/sealtiel/Global.java
+++ app/org/iatoki/judgels/sealtiel/Global.java
@@ -1,11 +1,16 @@
 package org.iatoki.judgels.sealtiel;
 
+import org.iatoki.judgels.sealtiel.services.impls.SealtielDataMigrationServiceImpl;
 import play.Application;
 import play.libs.F;
 import play.mvc.Http;
 import play.mvc.Result;
 
 public final class Global extends org.iatoki.judgels.play.Global {
+
+    public Global() {
+        super(new SealtielDataMigrationServiceImpl());
+    }
 
     @Override
     public void onStart(Application application) {
