0206f96dd88c318f1ce4876869f8f89674c24423
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-12-31 06:43:51 +0700

    Acquaintance migration

--- app/org/iatoki/judgels/sealtiel/SealtielDataMigrationServiceImpl.java
+++ app/org/iatoki/judgels/sealtiel/SealtielDataMigrationServiceImpl.java
@@ -6,15 +6,18 @@ import org.iatoki.judgels.play.migration.AbstractBaseDataMigrationServiceImpl;
 import play.db.jpa.JPA;
 
 import javax.inject.Singleton;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 
 @Singleton
 public final class SealtielDataMigrationServiceImpl extends AbstractBaseDataMigrationServiceImpl {
 
     @Override
     protected void onUpgrade(long databaseVersion, long codeDatabaseVersion) throws SQLException {
+        if (databaseVersion < 1) {
+            migrateV0toV1();
+        }
         if (databaseVersion < 2) {
             migrateV1toV2();
         }
@@ -36,4 +39,42 @@ public final class SealtielDataMigrationServiceImpl extends AbstractBaseDataMigr
         statement.execute("ALTER TABLE sealtiel_client DROP lastDownloadTime;");
         statement.execute("ALTER TABLE sealtiel_client DROP totalDownload;");
     }
+
+    private void migrateV0toV1() throws SQLException {
+        SessionImpl session = (SessionImpl) JPA.em().unwrap(Session.class);
+        Connection connection = session.getJdbcConnectionAccess().obtainConnection();
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM sealtiel_client");
+        while (resultSet.next()) {
+            String ipCreate = resultSet.getString("ipCreate");
+            String ipUpdate = resultSet.getString("ipUpdate");
+            Long timeCreate = resultSet.getLong("timeCreate");
+            Long timeUpdate = resultSet.getLong("timeUpdate");
+            String userCreate = resultSet.getString("userCreate");
+            String userUpdate = resultSet.getString("userUpdate");
+            String jid = resultSet.getString("jid");
+            String acquaintancesString = resultSet.getString("acquaintances");
+            List<String> acquaintancesList = Arrays.asList(acquaintancesString.split(","));
+
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO sealtiel_client_acquaintances (userCreate, timeCreate, ipCreate, userUpdate, timeUpdate, ipUpdate, clientJid, acquaintance) VALUES(?, ?, ?, ?, ?, ?, ?, ?);");
+            preparedStatement.setString(1, userCreate);
+            preparedStatement.setLong(2, timeCreate);
+            preparedStatement.setString(3, ipCreate);
+            preparedStatement.setString(4, userUpdate);
+            preparedStatement.setLong(5, timeUpdate);
+            preparedStatement.setString(6, ipUpdate);
+            preparedStatement.setString(7, jid);
+
+            for (String acquaintance : acquaintancesList) {
+                preparedStatement.setString(8, acquaintance);
+                preparedStatement.executeUpdate();
+            }
+
+        }
+
+        statement.execute("ALTER TABLE sealtiel_client DROP acquaintances");
+    }
+
 }
