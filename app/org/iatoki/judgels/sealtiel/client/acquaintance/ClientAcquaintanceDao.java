01c6ebba76cbeab4f7f2d30b88508d8133c0b87a
Ashar Fuadi <fushar@gmail.com>
2016-01-02 23:53:36 +0000

    Inject dao impls

--- app/org/iatoki/judgels/sealtiel/client/acquaintance/ClientAcquaintanceDao.java
+++ app/org/iatoki/judgels/sealtiel/client/acquaintance/ClientAcquaintanceDao.java
@@ -1,10 +1,11 @@
 package org.iatoki.judgels.sealtiel.client.acquaintance;
 
-
+import com.google.inject.ImplementedBy;
 import org.iatoki.judgels.play.model.Dao;
 
 import java.util.List;
 
+@ImplementedBy(ClientAcquaintanceHibernateDao.class)
 public interface ClientAcquaintanceDao extends Dao<Long, ClientAcquaintanceModel> {
 
     List<String> findAcquaintancesByClientJid(String clientJid);
--- app/org/iatoki/judgels/sealtiel/client/acquaintance/ClientAcquaintanceService.java
+++ app/org/iatoki/judgels/sealtiel/client/acquaintance/ClientAcquaintanceService.java
@@ -1,5 +1,8 @@
 package org.iatoki.judgels.sealtiel.client.acquaintance;
 
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(ClientAcquaintanceServiceImpl.class)
 public interface ClientAcquaintanceService {
     void addAcquaintance(String clientJid, String acquaintance);
 
