b6727da814447f12ea5bd61b8d07aaf13cd51bf8
Jordan Fernando <fernandojordan.92@gmail.com>
2016-01-07 01:16:05 +0700

    Refactor into getBaseHtmlTemplate

--- app/org/iatoki/judgels/sealtiel/AbstractSealtielController.java
+++ app/org/iatoki/judgels/sealtiel/AbstractSealtielController.java
@@ -1,17 +1,26 @@
 package org.iatoki.judgels.sealtiel;
 
-import org.iatoki.judgels.play.AbstractJudgelsController;
-import org.iatoki.judgels.play.template.HtmlTemplate;
+import org.iatoki.judgels.play.HtmlTemplate;
+import org.iatoki.judgels.play.controllers.AbstractJudgelsController;
+import org.iatoki.judgels.sealtiel.client.routes;
 import play.i18n.Messages;
 import play.mvc.Result;
 
 public abstract class AbstractSealtielController extends AbstractJudgelsController {
 
     @Override
-    protected Result renderTemplate(HtmlTemplate template) {
-        template.addSidebarMenu(Messages.get("client.text.clients"), org.iatoki.judgels.sealtiel.client.routes.ClientController.index());
-        template.addSidebarMenu(Messages.get("connection.text.connection"), org.iatoki.judgels.sealtiel.connection.routes.ConnectionController.index());
+    protected HtmlTemplate getBaseHtmlTemplate() {
+        HtmlTemplate htmlTemplate = super.getBaseHtmlTemplate();
+        htmlTemplate.addSidebarMenu(Messages.get("client.text.clients"), routes.ClientController.index());
+        htmlTemplate.addSidebarMenu(Messages.get("connection.text.connection"), routes.ConnectionController.index());
 
+        return htmlTemplate;
+    }
+
+    @Override
+    protected Result renderTemplate(HtmlTemplate template) {
+            template.addSidebarMenu(Messages.get("client.text.clients"), org.iatoki.judgels.sealtiel.client.routes.ClientController.index());
+            template.addSidebarMenu(Messages.get("connection.text.connection"), org.iatoki.judgels.sealtiel.connection.routes.ConnectionController.index());
         return super.renderTemplate(template);
     }
 }
