08850c727106f5ecf1cf627bd79e7de9e4991106
Ashar Fuadi <fushar@gmail.com>
2016-01-24 22:13:15 +0000

    Remove acquaintances from ClientModel_

--- app/org/iatoki/judgels/sealtiel/client/ClientModel.java
+++ app/org/iatoki/judgels/sealtiel/client/ClientModel.java
@@ -3,7 +3,6 @@ package org.iatoki.judgels.sealtiel.client;
 import org.iatoki.judgels.play.model.AbstractJudgelsModel;
 import org.iatoki.judgels.play.jid.JidPrefix;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -15,7 +14,4 @@ public final class ClientModel extends AbstractJudgelsModel {
     public String name;
 
     public String secret;
-
-    @Column(columnDefinition = "TEXT")
-    public String acquaintances;
 }
