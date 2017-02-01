e4bba4e1bf2032ceef570ac6229ff83071db74c8
Ashar Fuadi <fushar@gmail.com>
2017-02-01 21:53:39 +0000

    Properly set version

--- sealtiel-server/app/org/iatoki/judgels/sealtiel/SealtielModule.java
+++ sealtiel-server/app/org/iatoki/judgels/sealtiel/SealtielModule.java
@@ -3,6 +3,7 @@ package org.iatoki.judgels.sealtiel;
 import com.google.inject.AbstractModule;
 import org.iatoki.judgels.Config;
 import org.iatoki.judgels.play.ApplicationConfig;
+import org.iatoki.judgels.play.VersionUtils;
 import org.iatoki.judgels.play.general.GeneralName;
 import org.iatoki.judgels.play.general.GeneralVersion;
 import org.iatoki.judgels.play.migration.JudgelsDataMigrator;
@@ -14,7 +15,7 @@ public final class SealtielModule extends AbstractModule {
     @Override
     protected void configure() {
         bindConstant().annotatedWith(GeneralName.class).to("sealtiel");
-        bindConstant().annotatedWith(GeneralVersion.class).to("0.9.0");
+        bindConstant().annotatedWith(GeneralVersion.class).to(VersionUtils.getVersion());
         bindConstant().annotatedWith(QueueThreadPool.class).to(10);
 
         bind(Config.class).annotatedWith(AccountConfigSource.class).toInstance(ApplicationConfig.getInstance());
